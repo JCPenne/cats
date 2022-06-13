d5063b34a1e88b62665d941c472e85d905d99a5a
JCPenne <jackcpalmer@icloud.com>
2022-06-12 18:06:18 -0700

    add test files, install m&c, add text files for data acquisition

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, callbackFunc) {
+  console.log('Step 1: breedDetailsFromFile: Calling readFile...');
+
+  const dataReader = fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("Step 2: In readFile's Callback: it has the data.");
+    if (!error) callbackFunc(data);
+  });
+};
+const printOutCatBreed = breed => {
+  console.log(`Step 3 Return value ${breed}`);
+};
+
+const bombay = breedDetailsFromFile('Bombay', printOutCatBreed);
