77d151691debca24c8c324dac8c0b09dd76773e7
JCPenne <jackcpalmer@icloud.com>
2022-06-12 18:34:52 -0700

    working version of async test file for m&c. Fix bug with quotation marks in .txt file for accurate test result

--- async-breeds.js
+++ async-breeds.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function (breed, callbackFunc) {
-  console.log('Step 1: breedDetailsFromFile: Calling readFile...');
+  // console.log('Step 1: breedDetailsFromFile: Calling readFile...');
 
   const dataReader = fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("Step 2: In readFile's Callback: it has the data.");
-    if (!error) callbackFunc(data);
+    // console.log("Step 2: In readFile's Callback: it has the data.");
+    if (!error) {
+      callbackFunc(data);
+    }
+    if (error) {
+      callbackFunc(undefined);
+    }
   });
 };
 const printOutCatBreed = breed => {
-  console.log(`Step 3 Return value ${breed}`);
+  // console.log(`Step 3 Return value ${breed}`);
 };
-
-const bombay = breedDetailsFromFile('Bombay', printOutCatBreed);
 
 module.exports = breedDetailsFromFile;
